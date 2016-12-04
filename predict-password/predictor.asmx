cb0d0f78a4fcd8498488398e659f93c4084b5784
Sally Li <lisally@uw.edu>
2016-12-03 18:40:27 -0800

    added predictor.asmx for full data set

---
+++ predict-password/predictor.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="predictor.asmx.cs" Class="predict_password.predictor" %>
---
+++ predict-password/predictor.asmx.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Web.Script.Serialization;
+using System.Web.Script.Services;
+using System.Web.Services;
+
+namespace predict_password
+{
+    /// <summary>
+    /// Summary description for predictor
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    [System.Web.Script.Services.ScriptService]
+    public class predictor : System.Web.Services.WebService
+    {
+
+        private static List<string> passwords;
+        private static Dictionary<string, int> dictionary;
+        private string filePath = System.IO.Path.GetTempPath() + "\\data.txt";
+
+        [WebMethod]
+        public string DownloadData()
+
+        {
+            passwords = new List<string>();
+            dictionary = new Dictionary<string, int>();
+            File.Delete(this.filePath);
+
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+                CloudConfigurationManager.GetSetting("StorageConnectionString"));
+
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            CloudBlobContainer container = blobClient.GetContainerReference("info370");
+            {
+                CloudBlockBlob blob = container.GetBlockBlobReference("full_passwords.txt");
+
+                using (var fileStream = System.IO.File.OpenWrite(this.filePath))
+                {
+                    blob.DownloadToStream(fileStream);
+                }
+
+                //int count = 0;
+
+                using (StreamReader reader = new StreamReader(this.filePath))
+                {
+                    //PerformanceCounter counter = new PerformanceCounter("Memory", "Available MBytes");
+                    while (passwords.Count() < 20000000)
+                    {
+                        //if (count % 1000 == 0)
+                        //{
+                        //    if (counter.NextValue() < 100)
+                        //    {
+                        //        break;
+                        //    }
+                        //}
+                        string line = reader.ReadLine();
+                        passwords.Add(line);
+                        //count++;
+                    }
+                    
+                }
+
+                var searchDictionary = passwords.GroupBy(x => x)
+                .Select(x => new Tuple<string, int>(x.Key, x.Count()))
+                .ToList();
+
+                passwords = null;
+
+                for (int j = 0; j < searchDictionary.Count(); j++)
+                {
+                    dictionary.Add(searchDictionary[j].Item1, searchDictionary[j].Item2);
+                }
+
+                searchDictionary = null;
+
+            }
+            return "success downloading data";
+        }
+
+
+        [WebMethod]
+        [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+        public string SearchForPasswords(string search)
+        {
+            List<string> results = new List<string>();
+
+            var searchQuery = dictionary.Select(x => new Tuple<string, int>(x.Key, x.Value))
+                .Where(x => x.Item1.StartsWith(search))
+                .OrderByDescending(x => x.Item2)
+                .Take(10)
+                .ToList();
+
+            for (int i = 0; i < searchQuery.Count(); i++)
+            {
+                results.Add(searchQuery[i].Item1);
+                results.Add("" + searchQuery[i].Item2);
+
+            }
+
+            return new JavaScriptSerializer().Serialize(results);
+        }
+    }
+}
